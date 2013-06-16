45f1d08ac318a10be38078b807ddfbd6ead5cf5f
Arkadij Kuzhel <arkuzhel@gmail.com>
2013-06-16 05:41:35 +0300

    Report all errors

--- index.php
+++ index.php
@@ -3,7 +3,11 @@
 include 'app.php';
 
 define('LOG_ON', 1);
+/**
+ * Error reporting
+ */
+error_reporting(E_ALL | E_STRICT);
 
-$model = app::getModel('page');
+$model = app::getModel('page')->load(1);
 
 ?>
