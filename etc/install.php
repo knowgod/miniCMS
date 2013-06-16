580f600181240e328f89518277d422f1e97c8787
Arkadij Kuzhel <arkuzhel@gmail.com>
2013-06-16 05:41:21 +0300

    Data To be imported into DB on installation

---
+++ etc/install.php
@@ -0,0 +1,27 @@
+<?php
+
+$table_definitions = array(
+    'user_level' => array(
+        'table' => array(
+            self::PRIMARY_KEY => 'id',
+            'id' => 'int(10) unsigned NOT NULL auto_increment',
+            'level' => 'VARCHAR(32) NOT NULL',
+        ),
+        'data' => array(
+            array(1, 'guest'),
+            array(2, 'user'),
+            array(3, 'admin'),
+        ),
+    ),
+    'page' => array(
+        'table' => array(
+            self::PRIMARY_KEY => 'id',
+            'id' => 'int(10) unsigned NOT NULL auto_increment',
+            'path' => 'VARCHAR(16) NOT NULL',
+            'title' => 'VARCHAR(64) NOT NULL',
+            'content' => 'TEXT',
+            'user_level' => 'INT(10) NOT NULL DEFAULT 0',
+        ),
+    ),
+);
+?>
